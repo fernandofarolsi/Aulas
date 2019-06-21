6bc1244a2c424095bb79f18667862c76765e3b71
Fernando MAC <fernandofarolsi@gmail.com>
2019-06-20 21:46:36 -0700

    Feed feito (css nao sao importados)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import {Link } from 'react-router-dom';
+
 import './Header.css';
 
 import logo from '../assets/logo.svg';
 import camera from '../assets/camera.svg';
 
 
+
 export default function Header() {
   return (
-    <header id= "main-header">
+    <header id="main-header">
         <div className="header-content">
-            <img src="" alt="InstaRocket"/>
-            <img src="" alt="Enviar publicação" />
+            <Link to="/">
+                <img src={logo} alt="InstaRocket"/>
+            </Link>
+            <Link to="/new">
+            <img src={camera} alt="Enviar publicação" />
+            </Link>
         </div>
     </header>
   );
--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import './Feed.css';
 
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
 
 class Feed extends Component {
     render(){
         return(
-            <h1>Feed</h1>
+            <selection id="post-list">
+                <article>
+                    <header>
+                        <div className= "user-info">
+                            <span>Fernando Rodrigues</span>
+                            <span className="place">Rolim de Moura</span>
+                        </div>
+                        <img src={more} alt="Mais"/>
+                    </header>
+                    
+                    <img src="http://localhost:3333/files/D9RIS2OWkAAEC4A.jpg" alt="" />
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+
+                        <strong>90 Curtidas</strong>
+
+                        <p>Um posto muito top
+                            <span>#Topzera #ENoisQVoa</span>
+                        </p>
+                    </footer>
+
+                </article>
+
+            </selection>
+
         );
     }
 }
