bb8ce55ae113aa61e93e2737aea904689205b844
Fernando MAC <fernandofarolsi@gmail.com>
2019-06-25 15:38:30 -0700

    passando para o win10

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -36,6 +36,34 @@ class Feed extends Component {
                     </footer>
 
                 </article>
+                <article>
+                    <header>
+                        <div className= "user-info">
+                            <span>Fernando Rodrigues</span>
+                            <span className="place">Rolim de Moura</span>
+                        </div>
+                        <img src={more} alt="Mais"/>
+                    </header>
+                    
+                    <img src="http://localhost:3333/files/D9RIS2OWkAAEC4A.jpg" alt="" />
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+
+                        <strong>90 Curtidas</strong>
+
+                        <p>Um posto muito top
+                            <span>#Topzera #ENoisQVoa</span>
+                        </p>
+                    </footer>
+
+                </article>
+                
+
 
             </selection>
 
